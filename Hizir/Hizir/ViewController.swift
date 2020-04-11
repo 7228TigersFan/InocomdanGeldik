595e51833a7b35d63f631de4920d4a9bbdfcf966
Yalçın TÜR <yalcinturpg@gmail.com>
2020-04-11 23:18:18 +0300

    v0.3

--- Hizir/Hizir/ViewController.swift
+++ Hizir/Hizir/ViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tcKimlikTextField: UITextField!
+    
+    @IBOutlet weak var eDevletTextField: UITextField!
+    
+    var tcKimlik = "" as String
+       
+    var eDevlet = "" as String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
 
-
+    @IBAction func girisYapButton(_ sender: UIButton) {
+        
+           tcKimlik = tcKimlikTextField.text!
+           
+           eDevlet = eDevletTextField.text!
+        
+               let mainVC = storyboard?.instantiateViewController(withIdentifier: "MainVC")
+                            mainVC?.modalPresentationStyle = .fullScreen
+                                 present(mainVC!, animated: false, completion: nil)
+           
+           print(tcKimlik)
+           print(eDevlet)
+    }
+    
 }
 
